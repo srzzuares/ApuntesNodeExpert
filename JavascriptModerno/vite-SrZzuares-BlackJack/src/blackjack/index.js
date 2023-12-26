28e7c26dcca874ca2b4b11be5eb8ee46b973833e
sr <srzzuares@gmail.com>
2023-12-26 15:12:47 -0600

    blackjack vite listo y refactorizado

--- JavascriptModerno/vite-SrZzuares-BlackJack/src/blackjack/index.js
+++ JavascriptModerno/vite-SrZzuares-BlackJack/src/blackjack/index.js
@@ -1,4 +1,4 @@
-import { crearDeck } from './useCases/crear-deck';
+import { crearDeck, pedirCarta, valorCarta, turnoComputadora, crearCartaHTML } from './useCases'
 /**
  * 2C = Two of Clubs
  * 2D = Two of Diamonds
@@ -7,8 +7,7 @@ import { crearDeck } from './useCases/crear-deck';
  */
 
 let deck = [];
-const tipos = ['C', 'D', 'H', 'S'];
-const especiales = ['A', 'J', 'Q', 'K'];
+const tipos = ['C', 'D', 'H', 'S'], especiales = ['A', 'J', 'Q', 'K'];
 
 let puntosJugador = 0,
     puntosComputadora = 0;
@@ -17,97 +16,31 @@ let puntosJugador = 0,
 const btnPedir = document.querySelector('#btnPedir');
 const btnDetener = document.querySelector('#btnDetener');
 const btnNuevo = document.querySelector('#btnNuevo');
-
 const divCartasJugador = document.querySelector('#jugador-cartas');
 const divCartasComputadora = document.querySelector('#computadora-cartas');
-
 const puntosHTML = document.querySelectorAll('small');
-
-
-
-deck = crearDeck();
-
-
-// Esta función me permite tomar una carta
-const pedirCarta = () => {
-
-    if (deck.length === 0) {
-        throw 'No hay cartas en el deck';
-    }
-    const carta = deck.pop();
-    return carta;
-}
-
-// pedirCarta();
-const valorCarta = (carta) => {
-
-    const valor = carta.substring(0, carta.length - 1);
-    return (isNaN(valor)) ?
-        (valor === 'A') ? 11 : 10
-        : valor * 1;
-}
-
-// turno de la computadora
-const turnoComputadora = (puntosMinimos) => {
-
-    do {
-        const carta = pedirCarta();
-
-        puntosComputadora = puntosComputadora + valorCarta(carta);
-        puntosHTML[1].innerText = puntosComputadora;
-
-        // <img class="carta" src="assets/cartas/2C.png">
-        const imgCarta = document.createElement('img');
-        imgCarta.src = `cartas/${carta}.png`; //3H, JD
-        imgCarta.classList.add('carta');
-        divCartasComputadora.append(imgCarta);
-
-        if (puntosMinimos > 21) {
-            break;
-        }
-
-    } while ((puntosComputadora < puntosMinimos) && (puntosMinimos <= 21));
-
-    setTimeout(() => {
-        if (puntosComputadora === puntosMinimos) {
-            alert('Nadie gana :(');
-        } else if (puntosMinimos > 21) {
-            alert('Computadora gana')
-        } else if (puntosComputadora > 21) {
-            alert('Jugador Gana');
-        } else {
-            alert('Computadora Gana')
-        }
-    }, 100);
-}
-
-
+deck = crearDeck(tipos, especiales);
 
 // Eventos
 btnPedir.addEventListener('click', () => {
-
-    const carta = pedirCarta();
-
+    const carta = pedirCarta(deck);
     puntosJugador = puntosJugador + valorCarta(carta);
     puntosHTML[0].innerText = puntosJugador;
-
-    // <img class="carta" src="assets/cartas/2C.png">
-    const imgCarta = document.createElement('img');
-    imgCarta.src = `cartas/${carta}.png`; //3H, JD
-    imgCarta.classList.add('carta');
+    const imgCarta = crearCartaHTML(carta);
     divCartasJugador.append(imgCarta);
+
 
     if (puntosJugador > 21) {
         console.warn('Lo siento mucho, perdiste');
         btnPedir.disabled = true;
         btnDetener.disabled = true;
-        turnoComputadora(puntosJugador);
+        turnoComputadora(puntosJugador, puntosHTML[1], divCartasComputadora, deck);
 
     } else if (puntosJugador === 21) {
         console.warn('21, genial!');
         btnPedir.disabled = true;
         btnDetener.disabled = true;
-        turnoComputadora(puntosJugador);
+        turnoComputadora(puntosJugador, puntosHTML[1], divCartasComputadora, deck);
     }
 
 });
@@ -117,14 +50,14 @@ btnDetener.addEventListener('click', () => {
     btnPedir.disabled = true;
     btnDetener.disabled = true;
 
-    turnoComputadora(puntosJugador);
+    turnoComputadora(puntosJugador, puntosHTML[1], divCartasComputadora, deck);
 });
 
 btnNuevo.addEventListener('click', () => {
 
     console.clear();
     deck = [];
-    deck = crearDeck();
+    deck = crearDeck(tipos, especiales);
 
     puntosJugador = 0;
     puntosComputadora = 0;
