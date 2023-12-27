52e012904222254a0be1dd5563548abca61e00a6
sr <srzzuares@gmail.com>
2023-12-26 20:37:36 -0600

    ListaTarea

---
+++ JavascriptModerno/vite-SrZzuares-ListaTareas/src/todos/app.js
@@ -0,0 +1,12 @@
+import html from "./app.html?raw";
+/**
+ * 
+ * @param {String} elementId De tipo String
+ */
+export const App = (elementId) => {
+    (() => {
+        const app = document.createElement('div');
+        app.innerHTML = html;
+        document.querySelector(elementId).append(app);
+    })();
+}
