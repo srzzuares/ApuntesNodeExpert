629eb1a7d9394dee062a5b80c516e534bf819b5c
sr <srzzuares@gmail.com>
2024-01-02 16:12:03 -0600

    continuo en BreakingBad

---
+++ JavascriptModerno/PeticionesHttp-SrZzuares-js/main.js
@@ -0,0 +1,19 @@
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import { BreakingBad } from './src/breaking-bad/01-breakingBad'
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1 id="app-tittle">Hello SrZzuares!</h1>
+    <div class="card">
+    </div>
+  </div>
+`
+const elementHtml = document.querySelector('.card');
+BreakingBad(elementHtml);
---
+++ JavascriptModerno/PeticionesHttp-SrZzuares-js/src/breaking-bad/01-breakingBad.js
@@ -0,0 +1,39 @@
+
+/**
+ * 
+ * @returns {Object} quote information
+ */
+const fetchQuote = async () => {
+    const urlBreakbad = await fetch('https://api.breakingbadquotes.xyz/v1/quotes');
+    if (!urlBreakbad.ok) throw new Error(`HTTP error! status: ${urlBreakbad.status}`);
+    const data = await urlBreakbad.json();
+    console.log(data[0]);
+
+    return data[0];
+}
+
+
+/**
+ * 
+ * @param {HTMLElement} elementHtml 
+*/
+export const BreakingBad = (elementHtml) => {
+    document.querySelector('#app-tittle').innerHTML = 'Breaking App SrZzuares';
+    elementHtml.innerHTML = 'Loading...';
+
+    const quoteLabel = document.createElement('blockquote');
+    const autoLabel = document.createElement('h3');
+    const nextQuoteBtn = document.createElement('button');
+    nextQuoteBtn.innerHTML = 'Next Quote!';
+
+    const renderQuote = (data) => {
+        quoteLabel.innerHTML = data.quote;
+        autoLabel.innerHTML = data.author;
+        elementHtml.replaceChildren(quoteLabel, autoLabel, nextQuoteBtn);
+
+    }
+
+    fetchQuote()
+        .then(renderQuote);
+
+}
